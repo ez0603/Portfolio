74bc03a93152bd3f46cea9d5fb3182c55f8de971
ez0603 <dlwldjs3132@naver.com>
2024-07-04 22:12:09 +0900

    Feat : 프로필 작성

--- src/pages/ProfileMain/ProfileMain.jsx
+++ src/pages/ProfileMain/ProfileMain.jsx
@@ -1,17 +1,54 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
-
+import Image from "../../assets/img/file.jpg";
+import backImage from "../../assets/img/background.jpg";
+import { MdMailOutline } from "react-icons/md";
+import { useState } from "react";
 
 function ProfileMain(props) {
-	return (
-		<div css={s.layout}>
-			<div css={s.container}>
-				<div css={s.content}>
-					fsdffsd
-				</div>
-			</div>
-		</div>
-	);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyEmail = () => {
+    const email = "dlwldjs31232@naver.com";
+    navigator.clipboard.writeText(email).then(() => {
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000); // 2초 후에 알림 메시지 숨김
+    });
+  };
+
+  return (
+    <div css={s.layout}>
+      <div css={s.container}>
+        <div css={s.content}>
+          <div css={s.profileContainer}>
+            <div css={s.aboutMe}>
+              <div css={s.background}>
+                <img src={backImage} alt="" />
+              </div>
+              <div css={s.img}>
+                <img src={Image} alt="" />
+                <h3>이지언</h3>
+                <div css={s.messageBox}>
+                  <MdMailOutline
+                    size={50}
+                    color="white"
+                    onClick={handleCopyEmail}
+                  />
+                  <p>Email</p>
+                  {isCopied && (
+                    <p css={s.copiedMessage}>Email copied to clipboard!</p>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div>
+              <h1>Profile</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileMain;
--- src/pages/ProfileMain/style.js
+++ src/pages/ProfileMain/style.js
@@ -2,25 +2,109 @@ import { css } from "@emotion/react";
 
 export const layout = css`
   position: relative;
-  width: 100%; //모니터 기준
+  width: 100%;
   height: 93vh;
-  background-color: #ecc7c7;
+  background-color: #fff;
   display: flex;
   align-items: center;
-  margin-top: 64px;
+  margin-top: 63px;
 `;
 
 export const container = css`
   width: 100%;
   height: 100%;
-  background-color: aqua;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
 export const content = css`
-  width: 90%;
+  width: 100%;
   height: 100%;
-  background-color: orange;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const profileContainer = css`
+  overflow: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  width: 100%;
+  height: 100%;
+  /* background-color: aqua; */
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const aboutMe = css`
+  /* background-color: orange; */
+  width: 30%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  border-radius: 20px;
+  position: relative;
+  flex-direction: column;
+`;
+
+export const background = css`
+  background-color: #fff;
+  width: 70%;
+  height: 95%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  & img {
+    width: 100%;
+    border-radius: 25px;
+    height: 100%;
+    opacity: 0.5;
+  }
+`;
+
+export const img = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 50%;
+  left: 35%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: end;
+
+  & img {
+    border-radius: 40px;
+    width: 150px;
+    height: 150px;
+  }
+
+  & h3 {
+    font-weight: 700;
+    color: white;
+    margin-top: 10px;
+  }
+`;
+
+export const messageBox = css`
+  background-color: transparent;
+  width: 100%;
+  height: 25%;
+  border-top: 1px solid white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: white;
+`;
+
+export const copiedMessage = css`
+  color: yellow;
+  font-size: 14px;
+  margin-top: 10px;
 `;
