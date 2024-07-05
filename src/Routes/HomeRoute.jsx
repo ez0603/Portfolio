65cd1e09b9e9bcd6c8c6050e7ace2c32332d6f3f
ez0603 <dlwldjs3132@naver.com>
2024-07-05 20:16:42 +0900

    Fix : package.json  파일 수정

--- src/Routes/HomeRoute.jsx
+++ src/Routes/HomeRoute.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ProfileMain from '../pages/ProfileMain/ProfileMain';
 import Home from '../pages/Home/Home';
 import Project from '../pages/Project/Project';
 
-function HomeRoute(props) {
+function HomeRoute() {
   return (
     <>
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Profile" element={<ProfileMain />} />
-      <Route path="/Project" element={<Project />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Profile" element={<ProfileMain />} />
+        <Route path="/Project" element={<Project />} />
+      </Routes>
     </>
   );
 }
