4039cf9bfeefb8458fd4eec095c7baba06e960a2
ez0603 <dlwldjs3132@naver.com>
2024-07-05 20:04:16 +0900

    Fix : package.json  파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import "./App.css";
 import HomeRoute from "./Routes/HomeRoute";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/Portfolio">
+    <>
       <Header />
       <HomeRoute />
       <Footer />
-    </Router>
+    </>
   );
 }
 
