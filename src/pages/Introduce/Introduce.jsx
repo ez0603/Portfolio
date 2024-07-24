57828b6542a4a8ad4073bcecb90ad1e006ee70cb
ez0603 <dlwldjs3132@naver.com>
2024-07-24 15:59:13 +0900

    Feat : 타이핑 애니메이션 추가

--- src/pages/Introduce/Introduce.jsx
+++ src/pages/Introduce/Introduce.jsx
@@ -1,26 +1,49 @@
 /** @jsxImportSource @emotion/react */
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as s from "./style";
 
 function Introduce() {
   const content = "안녕하세요\n웹 개발자 이지언입니다";
   const [displayText, setDisplayText] = useState('');
   const [index, setIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
-    if (index < content.length) {
-      const timer = setTimeout(() => {
-        setDisplayText(prev => prev + (content[index] === "\n" ? "<br/>" : content[index]));
-        setIndex(index + 1);
-      }, 100); // Typing speed
+    let timer;
 
-      return () => clearTimeout(timer);
+    if (isDeleting) {
+      if (index >= 0) {
+        timer = setTimeout(() => {
+          setDisplayText((prev) => prev.slice(0, index));
+          setIndex(index - 1);
+        }, 100); // Speed of deleting
+      } else {
+        // 완전히 삭제 완료 후, 초기화
+        timer = setTimeout(() => {
+          setDisplayText(''); // 남은 모든 텍스트 제거
+          setIndex(0); // 초기화
+          setIsDeleting(false); // 타이핑 모드로 전환
+        }, 1000); // 다음 타이핑 전 대기 시간
+      }
+    } else {
+      if (index < content.length) {
+        timer = setTimeout(() => {
+          setDisplayText((prev) => prev + content[index]);
+          setIndex(index + 1);
+        }, 100); // Typing speed
+      } else {
+        timer = setTimeout(() => {
+          setIsDeleting(true);
+        }, 2000); // 삭제 전 대기 시간
+      }
     }
-  }, [index, content]);
+
+    return () => clearTimeout(timer);
+  }, [index, isDeleting, content]);
 
   return (
     <div css={s.layout}>
-      <div css={s.introduce} dangerouslySetInnerHTML={{ __html: displayText + '<span class="cursor">|</span>' }} />
+      <div css={s.introduce} dangerouslySetInnerHTML={{ __html: displayText.replace(/\n/g, '<br/>') + '<span class="cursor">|</span>' }} />
     </div>
   );
 }
